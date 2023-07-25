769b3e6dc254f0037160e4320e0bc810b5941f31
jwooklee <jaewook04@gmail.com>
2023-07-25 23:05:45 +0900

    [START] Project Setting

---
+++ week13_assignment/assignment/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import MyPage from "./pages/MyPage";
+import Layout from "./components/layout/Layout";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/mypage" element={<MyPage />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+};
+
+export default App;
